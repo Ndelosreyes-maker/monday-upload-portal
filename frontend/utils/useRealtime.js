742af04fcaca4cb40380c334137d4d3cb758ea28
Ndelosreyes-maker <Ndelosreyes@legendarytherapy.com>
2026-02-25 23:43:16 +0800

    Update useRealtime.js

--- frontend/utils/useRealtime.js
+++ frontend/utils/useRealtime.js
@@ -1,1 +1,10 @@
 
+import { useEffect } from "react";
+
+export default function useRealtime(cb){
+  useEffect(()=>{
+    const ws=new WebSocket(process.env.NEXT_PUBLIC_API_URL.replace("https","wss"));
+    ws.onmessage=e=>cb(JSON.parse(e.data));
+    return ()=>ws.close();
+  },[]);
+}
