dc8d338f7c741aa934b28305cf63224ae14d4a09
Ndelosreyes-maker <Ndelosreyes@legendarytherapy.com>
2026-02-25 23:40:42 +0800

    Update monday.js

--- backend/monday.js
+++ backend/monday.js
@@ -1,1 +1,16 @@
 
+import axios from "axios";
+
+export const mondayClient = async (query, variables = {}) => {
+  const res = await axios.post(
+    "https://api.monday.com/v2",
+    { query, variables },
+    {
+      headers: {
+        Authorization: process.env.MONDAY_API_KEY,
+        "Content-Type": "application/json"
+      }
+    }
+  );
+  return res.data;
+};
