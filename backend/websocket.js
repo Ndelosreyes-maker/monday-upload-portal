d5d9532f8fc759376f7d61eeed39b28b4c20f14c
Ndelosreyes-maker <Ndelosreyes@legendarytherapy.com>
2026-02-25 23:40:55 +0800

    Update websocket.js

--- backend/websocket.js
+++ backend/websocket.js
@@ -1,1 +1,17 @@
 
+import WebSocket from "ws";
+
+let wss;
+
+export const initWebSocket = (server) => {
+  wss = new WebSocket.Server({ server });
+};
+
+export const notifyClients = (msg) => {
+  if (!wss) return;
+  wss.clients.forEach(c => {
+    if (c.readyState === WebSocket.OPEN) {
+      c.send(JSON.stringify(msg));
+    }
+  });
+};
